3b25bd0ab060a1db9e83348d6b00df03eda8dd63
McKinnin Lloyd <lloydmckinnin@gmail.com>
2020-11-09 08:31:10 -0700

    Working on hocks

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 /** @jsxImportSource @emotion/core */
 import { css, jsx } from '@emotion/core';
 import { PrimaryButton } from './Styles';
@@ -8,42 +9,47 @@ import { Page } from './Page';
 import { PageTitle } from './PageTitle';
 
 
-export const HomePage = () => (
-  <Page>
-    <div
-      css=
-      {
-        css`
-        margin: 50px auto 20px auto;
-        padding: 30px 20px;
-        max-width: 600px;`
-      }
-    >
+export const HomePage = () => {
+  useEffect(() => {
+    console.log('first rendered');
+    }, []);
+  return (
+    <Page>
       <div
         css=
         {
           css`
-          display: flex;
-          align-items: center;
-          justify-content: space-between;` 
+          margin: 50px auto 20px auto;
+          padding: 30px 20px;
+          max-width: 600px;`
         }
       >
-        <h2
+        <div
           css=
           {
             css`
-            font-size: 15px;
-            font-weight: bold;
-            margin: 10px 0px 5px;
-            text-align: center;
-            text-transform: uppercase;`
+            display: flex;
+            align-items: center;
+            justify-content: space-between;` 
           }
-        > 
-          <PageTitle>Unanswered Questions</PageTitle>
-        </h2>
-        <PrimaryButton>Ask a question</PrimaryButton>
+        >
+          <h2
+            css=
+            {
+              css`
+              font-size: 15px;
+              font-weight: bold;
+              margin: 10px 0px 5px;
+              text-align: center;
+              text-transform: uppercase;`
+            }
+          > 
+            <PageTitle>Unanswered Questions</PageTitle>
+          </h2>
+          <PrimaryButton>Ask a question</PrimaryButton>
+        </div>
+        {/* <QuestionList data={getUnansweredQuestions()} /> */}
       </div>
-      <QuestionList data={getUnansweredQuestions()} />
-    </div>
-  </Page>
-);
+    </Page>
+  );
+};
