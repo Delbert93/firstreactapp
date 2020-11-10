84f0b8597c269b8a728945171225dea3b70fd8a0
mckinnin <lloydmckinnin@gmail.com>
2020-11-10 16:26:53 -0700

    Working on forms still.

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import React, { FC, useState } from 'react';
 import { PrimaryButton, gray5, gray6 } from './Styles';
 /** @jsxImportSource @emotion/core */
 import { css, jsx } from '@emotion/core';
@@ -11,5 +11,34 @@ interface Props {
     submitCaption?: string;
 }
 
-export const Form: FC<Props> = ({ submitCaption, children}) =>
-    null;
+export const Form: FC<Props> = ({ submitCaption, children }) => {
+    const [values, setValues] = useState<Values>({});
+    return (
+        <form noValidate={true}>
+          <fieldset
+            css={css`
+              margin: 10px auto 0 auto;
+              padding: 30px;
+              width: 350px;
+              background-color: ${gray6};
+              border-radius: 4px;
+              border: 1px solid ${gray5};
+              box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+            `}
+          >
+            {children}
+            <div
+              css={css`
+                margin: 30px 0px 0px 0px;
+                padding: 20px 0px 0px 0px;
+                border-top: 1px solid ${gray5};
+              `}
+            >
+              <PrimaryButton type="submit">
+                {submitCaption}
+              </PrimaryButton>
+            </div>
+          </fieldset>
+        </form>
+      );      
+  };
